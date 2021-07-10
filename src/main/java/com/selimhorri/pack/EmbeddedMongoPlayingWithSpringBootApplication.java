95579763c099162f768519395fc2d22ae4c2ce1a
SelimHorri <horri.slim@gmail.com>
2021-07-10 14:38:27 +0100

    init

---
+++ src/main/java/com/selimhorri/pack/EmbeddedMongoPlayingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedMongoPlayingWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbeddedMongoPlayingWithSpringBootApplication.class, args);
+	}
+
+}
