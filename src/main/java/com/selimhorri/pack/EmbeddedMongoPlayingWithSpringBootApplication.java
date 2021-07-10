17e2a32c28422cae1bcff75989fd98f917c42996
SelimHorri <horri.slim@gmail.com>
2021-07-10 15:11:38 +0100

    add commandlinerunner to be implemented

--- src/main/java/com/selimhorri/pack/EmbeddedMongoPlayingWithSpringBootApplication.java
+++ src/main/java/com/selimhorri/pack/EmbeddedMongoPlayingWithSpringBootApplication.java
@@ -1,13 +1,28 @@
 package com.selimhorri.pack;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmbeddedMongoPlayingWithSpringBootApplication {
+public class EmbeddedMongoPlayingWithSpringBootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmbeddedMongoPlayingWithSpringBootApplication.class, args);
 	}
-
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
