baceef03bd3bac87e6c89b57aa2c8850ad9d5d1a
SelimHorri <horri.slim@gmail.com>
2021-07-11 14:15:36 +0100

    expose basic apis

--- src/main/java/com/selimhorri/pack/resource/DepartmentResource.java
+++ src/main/java/com/selimhorri/pack/resource/DepartmentResource.java
@@ -1,10 +1,15 @@
 package com.selimhorri.pack.resource;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +20,36 @@ import com.selimhorri.pack.repo.DepartmentRepository;
 @RequestMapping("/app/api/departments")
 public class DepartmentResource {
 	
-	@Autowired
 	private DepartmentRepository departmentRepository;
+	
+	public DepartmentResource(final DepartmentRepository departmentRepository) {
+		this.departmentRepository = departmentRepository;
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Department>> findAll() {
-		return ResponseEntity.ok(this.departmentRepository.findAll());
+		return new ResponseEntity<>(this.departmentRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Department> findById(@PathVariable("id") final String id) {
+		return new ResponseEntity<>(this.departmentRepository.findById(id).orElseThrow(NoSuchElementException::new), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Department> save(final Department department) {
+		return new ResponseEntity<>(this.departmentRepository.save(department), HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Department> update(final Department department) {
+		return new ResponseEntity<>(this.departmentRepository.save(department), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> deleteById(@PathVariable("id") final String id) {
+		this.departmentRepository.deleteById(id);
+		return new ResponseEntity<>(true, HttpStatus.OK);
 	}
 	
 	
--- src/main/java/com/selimhorri/pack/resource/EmployeeResource.java
+++ src/main/java/com/selimhorri/pack/resource/EmployeeResource.java
@@ -1,10 +1,15 @@
 package com.selimhorri.pack.resource;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +29,27 @@ public class EmployeeResource {
 	@GetMapping
 	public ResponseEntity<List<Employee>> findAll() {
 		return new ResponseEntity<>(this.employeeRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Employee> findById(@PathVariable("id") final String id) {
+		return new ResponseEntity<>(this.employeeRepository.findById(id).orElseThrow(NoSuchElementException::new), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Employee> save(final Employee employee) {
+		return new ResponseEntity<>(this.employeeRepository.save(employee), HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Employee> update(final Employee employee) {
+		return new ResponseEntity<>(this.employeeRepository.save(employee), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> deleteById(@PathVariable("id") final String id) {
+		this.employeeRepository.deleteById(id);
+		return new ResponseEntity<>(true, HttpStatus.OK);
 	}
 	
 	
